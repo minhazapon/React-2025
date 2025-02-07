10e18b462c62faab5802b8122615d6d4ef782c49
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-06 20:58:30 -0800

    added file

--- src/conditional rendering/CR.jsx
+++ src/conditional rendering/CR.jsx
@@ -1,30 +1,23 @@
 
-
-
 function CR() {
 
     const RiverInfo = {
-
         name: 'Navaganga',
         location: 'Jhenaidah',
         country: 'Bangladesh',
         CreatePlace: 'Gorai nodi'
-
     }
 
     let age = 20
 
     const Re = () => {
-
         if (age > 18) return "Yes Info available"
         return "So sad Not Available"
-
     }
 
     return (
         <>
             <div className=" mt-10 mb-10 ml-16 mr-16 flex justify-center ">
-
                 <div className=" flex justify-center items-center gap-5 border-[1px] w-fit p-8 ">
                     <img className=" h-[300px] w-[400px] rounded-xl " src="https://upload.wikimedia.org/wikipedia/commons/b/b9/Nabaganga_River.jpg" alt="" />
                     <div>
@@ -38,7 +31,6 @@ function CR() {
                     </div>
                 </div>
             </div>
-
         </>
     )
 }
