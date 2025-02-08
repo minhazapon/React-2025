ed2ea71f84b8e0c48e2dc03a98045cfd3f1810c9
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-07 20:01:31 -0800

    added file

--- src/LogoTitle.jsx
+++ src/LogoTitle.jsx
@@ -1,30 +1,33 @@
 
 function LogoTitle() {
     return (
-        <div className=" p-5 ">
-            <div className=" flex justify-center ">
-                <img className=" h-[50px] " src="https://cdn-icons-png.flaticon.com/128/2079/2079154.png" alt="" />
-            </div>
-            <div className=" flex justify-center mt-2 ">
-                <div className="rating rating-sm">
-                    <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-400" />
-                    <input
-                        type="radio"
-                        name="rating-6"
-                        className="mask mask-star-2 bg-orange-400"
-                        defaultChecked />
-                    <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-400" />
-                    <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-400" />
-                    <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-400" />
+        <>
+            <div className=" p-5 ">
+                <div className=" flex justify-center ">
+                    <img className=" h-[50px] " src="https://cdn-icons-png.flaticon.com/128/2079/2079154.png" alt="" />
+                </div>
+                <div className=" flex justify-center mt-2 ">
+                    <div className="rating rating-sm">
+                        <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-400" />
+                        <input
+                            type="radio"
+                            name="rating-6"
+                            className="mask mask-star-2 bg-orange-400"
+                            defaultChecked />
+                        <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-400" />
+                        <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-400" />
+                        <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-400" />
+                    </div>
+                </div>
+                <div>
+                    <p className=" text-center font-thin text-xl mt-3 text-gray-400 ">Food & Drinks</p>
+                </div>
+                <div className=" mt-5 ">
+                    <hr></hr>
                 </div>
             </div>
-            <div>
-                <p className=" text-center font-thin text-xl mt-3 text-gray-400 ">Food & Drinks</p>
-            </div>
-            <div className=" mb-5 mt-5 ">
-                <hr></hr>
-            </div>
-        </div>
+        </>
+
     )
 }
 
--- src/ResNav.jsx
+++ src/ResNav.jsx
@@ -1,9 +1,56 @@
+import { NavLink } from "react-router-dom"
 
 
 function ResNav() {
-    return (
-        <div>
 
+    const RNav = <>
+
+        <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink to='/About'>About</NavLink></li>
+        <li><NavLink to='/Page'>Page</NavLink></li>
+        <li><NavLink to='/Menu'>Menu</NavLink></li>
+        <li><NavLink to='/Shop'>Shop</NavLink></li>
+        <li><NavLink to='/News'>News</NavLink></li>
+        <li><NavLink to='/Contact'>Contact</NavLink></li>
+
+    </>
+
+
+    return (
+        <div className="">
+            <div className="navbar flex justify-center bg-base-100">
+                <div className="navbar-center ">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16" />
+                            </svg>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            {RNav}
+                        </ul>
+                    </div>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {RNav}
+                    </ul>
+                </div>
+                {/* <div className="navbar-end">
+                    <a className="btn">Button</a>
+                </div> */}
+            </div>
         </div>
     )
 }
